0726674da3d644300b5798f055568ad304205328
Jan Thurau <jan@janthurau.de>
2017-03-11 17:54:54 +0100

    compiled it down ..

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 'use strict';
-const Promise = require('bluebird');
-const streamToPromise = require('stream-to-promise');
-const rp = require('request-promise');
 
-const url = 'https://graph-video.facebook.com';
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var Promise = require('bluebird');
+var streamToPromise = require('stream-to-promise');
+var rp = require('request-promise');
+
+var url = 'https://graph-video.facebook.com';
 
 function apiInit(args, videoSize) {
-	const options = {
+	var options = {
 		method: 'POST',
-		uri: `${url}/v2.6/${args.id}/videos?access_token=${args.token}`,
+		uri: url + '/v2.6/' + args.id + '/videos?access_token=' + args.token,
 		json: true,
-		form: {
+		form: _extends({
 			upload_phase: 'start',
-			file_size: videoSize,
-			...args['initParameters']
-		}
+			file_size: videoSize
+		}, args['initParameters'])
 	};
 
 	return rp(options);
 }
 
 function apiFinish(args, id, video_id) {
-	const options = {
+	var options = {
 		method: 'POST',
-		uri: `${url}/v2.6/${args.id}/videos`,
+		uri: url + '/v2.6/' + args.id + '/videos',
 		form: {
 			access_token: args.token,
 			upload_phase: 'finish',
@@ -32,15 +34,14 @@ function apiFinish(args, id, video_id) {
 		json: true
 	};
 
-	return rp(options)
-		.then(res => {
-			res.video_id = video_id;
-			return res;
-		});
+	return rp(options).then(function (res) {
+		res.video_id = video_id;
+		return res;
+	});
 }
 
 function uploadChunk(args, id, start, chunk) {
-	const formData = {
+	var formData = {
 		access_token: args.token,
 		upload_phase: 'transfer',
 		start_offset: start,
@@ -52,9 +53,9 @@ function uploadChunk(args, id, start, chunk) {
 			}
 		}
 	};
-	const options = {
+	var options = {
 		method: 'POST',
-		uri: `${url}/v2.6/${args.id}/videos`,
+		uri: url + '/v2.6/' + args.id + '/videos',
 		formData: formData,
 		json: true
 	};
@@ -67,19 +68,22 @@ function uploadChain(buffer, args, res, ids) {
 		return ids;
 	}
 	var chunk = buffer.slice(res.start_offset, res.end_offset);
-	return uploadChunk(args, ids[0], res.start_offset, chunk)
-	.then(res => uploadChain(buffer, args, res, ids));
+	return uploadChunk(args, ids[0], res.start_offset, chunk).then(function (res) {
+		return uploadChain(buffer, args, res, ids);
+	});
 }
 
 function facebookApiVideoUpload(args) {
-	return Promise.resolve(streamToPromise(args.stream))
-		.then(buffer => buffer)
-		.then(buffer => [buffer, apiInit(args, buffer.length)])
-		.spread((buffer, res) => {
-			const ids = [res.upload_session_id, res.video_id];
-			return uploadChain(buffer, args, res, ids);
-		})
-		.spread((id, video_id) => apiFinish(args, id, video_id));
+	return Promise.resolve(streamToPromise(args.stream)).then(function (buffer) {
+		return buffer;
+	}).then(function (buffer) {
+		return [buffer, apiInit(args, buffer.length)];
+	}).spread(function (buffer, res) {
+		var ids = [res.upload_session_id, res.video_id];
+		return uploadChain(buffer, args, res, ids);
+	}).spread(function (id, video_id) {
+		return apiFinish(args, id, video_id);
+	});
 }
 
 module.exports = facebookApiVideoUpload;
